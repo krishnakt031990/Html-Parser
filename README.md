bfec900b60fa1d1a0386a0ab3b3ec84b340d9291
krishnateja <krishna.kt03@gmail.com>
2016-02-06 23:03:14 -0700

    Dont like dirty front end

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Html-Parser
 Challenge
+
+
+Tested this app with a lot of urls from cnn.com. Here is a sample url you can plug in a play.
+
+http://www.cnn.com/2016/02/06/politics/republican-debate-highlights/index.html
--- index.php
+++ index.php
@@ -2,10 +2,11 @@
 <title>CNN Url Parser</title>
 <body>
 
+<h2>Put in that URL and see the magic!</h2>
+
 <form action="parser.php" method="post">
-    What is url of the page you would want to parse?
-    <input type="text" name="cnnurl">
-    <input type="submit" name="formSubmit" value="Submit">
+	<input type="text" name="cnnurl" size="65">
+	<input type="submit" name="formSubmit" value="Submit">
 </form>
 
 </body>
